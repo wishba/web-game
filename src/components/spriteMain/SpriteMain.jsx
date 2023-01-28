43da6d3df3e91c90c7d90b7c099d9ed0e1608f4d
wishba <wisnubayu34@gmail.com>
2023-01-28 12:46:59 +0700

    zoom

--- src/components/spriteMain/SpriteMain.jsx
+++ src/components/spriteMain/SpriteMain.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react'
-import imgSpriteMain from './sprite-main.png'
+import spriteMainImg from './sprite-main.png'
 
 function SpriteMain() {
 
   const [positionX, setPositionX] = useState(0)
   const [positionY, setPositionY] = useState(0)
 
+  const spriteMainWidth = '14px'
+  const spriteMainHeight = '14px'
+
+  const zoomSize = '5'
+
   const style = {
     position: 'absolute',
     left: `calc(50% + ${positionX}px)`,
     top: `calc(50% + ${positionY}px)`,
     transform: 'translate(-50%, -50%)',
+
+    width: `calc(${spriteMainWidth} * ${zoomSize})`,
+    height: `calc(${spriteMainHeight} * ${zoomSize})`,
   }
 
   return (
     <>
-      <img style={style} src={imgSpriteMain} alt="sprite main" />
+      <img style={style} src={spriteMainImg} alt="sprite main" />
       <button onClick={() => setPositionY(positionY - 1)}>up</button>
       <button onClick={() => setPositionX(positionX + 1)}>right</button>
       <button onClick={() => setPositionY(positionY + 1)}>down</button>
