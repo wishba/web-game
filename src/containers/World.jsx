3297d755ecd23580f3779f12559c109a0c27d327
wishba <wisnubayu34@gmail.com>
2023-02-20 22:04:08 +0700

    log coordinate

--- src/containers/World.jsx
+++ src/containers/World.jsx
@@ -9,12 +9,19 @@ import './Word.css'
 
 function World() {
   const worldBox = useRef(null)
+
+  function handleScroll() {
+    const scrollX = worldBox.current.scrollTop
+    const scrollY = worldBox.current.scrollLeft
+    console.log(`X:${scrollX} Y:${scrollY}`)
+  }
+
   useEffect(() => {
     worldBox.current.focus()
   }, [])
 
   return (
-    <div className='world' ref={worldBox} tabIndex={0}>
+    <div className='world' ref={worldBox} tabIndex={0} onScroll={handleScroll}>
       <img className='map' src={map} alt="map" />
       <img className='character' src={character} alt="character" />
     </div>
