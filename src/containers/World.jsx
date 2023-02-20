7036b532de5fe4375d27f38d349efb115c75d316
wishba <wisnubayu34@gmail.com>
2023-02-20 20:11:04 +0700

    focus on load

--- src/containers/World.jsx
+++ src/containers/World.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import character from '../assets/character.png'
 import map from '../assets/map.png'
 import './Word.css'
 
 function World() {
+  const worldBox = useRef(null)
+  useEffect(() => {
+    worldBox.current.focus()
+  }, [])
+
   return (
-    <div className='world'>
+    <div className='world' ref={worldBox} tabIndex={0}>
       <img className='map' src={map} alt="map" />
       <img className='character' src={character} alt="character" />
     </div>
