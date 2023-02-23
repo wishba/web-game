0fc8ed7d79aef4211471cbcd52c0d802a5a37d22
wishba <wisnubayu34@gmail.com>
2023-02-23 09:15:49 +0700

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import hero from '../assets/Basic Charakter Spritesheet.png'
+
+function Hero() {
+  return (
+    <>
+      <img src={hero} alt="hero" />
+    </>
+  )
+}
+
+export default Hero
