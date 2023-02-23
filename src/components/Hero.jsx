940c07ed9db72e9df64743a245dfe0f5e0e7019b
wishba <wisnubayu34@gmail.com>
2023-02-23 14:09:22 +0700

    moved to app

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
 import './Hero.css'
 import hero from '../assets/Basic Charakter Spritesheet.png'
 
 function Hero() {
-  const heroRef = useRef(null);
-
-  useEffect(() => {
-    const { offsetLeft, offsetTop } = heroRef.current;
-    console.log(`hero, position ${offsetLeft}/${offsetTop}`);
-  }, []);
-
-
   return (
-    <div ref={heroRef} className='hero'>
+    <div className='hero'>
       <img className='hero__sprite' src={hero} alt="hero" />
     </div>
   )
