8f210360b0e11520562bbb2fdea8e3f9f29b9cb0
wishba <wisnubayu34@gmail.com>
2023-02-23 09:16:13 +0700

    comments out the css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import Hero from './components/Hero'
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Hero />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
