0036de372ec0e3cf782ec43cb2bef6b4d78ee44b
wishba <wisnubayu34@gmail.com>
2023-02-24 11:48:57 +0700

    grid auto auto

--- src/containers/MapGrid.jsx
+++ src/containers/MapGrid.jsx
@@ -7,72 +7,42 @@ function MapGrid() {
   const tileZoom = tile * zoom
 
   const grid = useRef(null)
-  const [tileSets, setTileSets] = useState([])
 
+  const [tileSets, setTileSets] = useState([])
   const [tileRows, setTileRows] = useState(0)
   const [tileColumns, setTileColumns] = useState(0)
-  console.log(tileRows);
+
+  const gridAuto = tileColumns - 1
+  console.log(tileColumns);
+  // console.log(gridAuto);
 
   useEffect(() => {
     if (grid.current) {
       const tileInWidth = Math.floor(grid.current.offsetWidth / tileZoom)
-      // setTileInWidth(Math.floor(grid.current.offsetWidth / tileZoom))
-      // setTileInWidth(Math.floor(grid.current.offsetWidth / tileZoom))
       const tileInHeight = Math.floor(grid.current.offsetHeight / tileZoom)
 
       setTileColumns(tileInHeight)
-      setTileRows(tileInWidth - tileColumns + 1)
-
-
-      // console.log(Math.floor(grid.current.offsetWidth / tileZoom));
-      // console.log(tileInWidth);
-
-      // console.log(`screen size: ${grid.current.offsetWidth}/${grid.current.offsetHeight}`);
-      // console.log(`tile: ${tileInWidth} x ${tileInHeight}`);
+      setTileRows(tileInWidth - tileColumns)
 
       const tile = []
       for (let indexW = 0; indexW < tileInWidth; indexW++) {
         for (let indexH = 0; indexH < tileInHeight; indexH++) {
-          // console.log(`${indexW}w/${indexH}h`);
           tile.push(<div key={`${indexW}w/${indexH}h`}>{`${indexW}w/${indexH}h`}</div>)
         }
       }
       setTileSets(tile)
-
-      // const gridVertical = []
-      // let indexVertical = 0
-      // for (const iterator of Array.from({ length: tileInHeight })) {
-      //   for (const iterator of Array.from({ length: tileInHeight })) {
-      //   gridVertical.push(<div key={indexVertical}>{indexVertical}v</div>)
-      //   indexVertical++
-      //   console.log(typeof iterator);
-      // }
-
-      // const gridHorizontal = []
-      // let indexHorizontal = 0
-      // for (const iterator of Array.from({ length: tileInWidth })) {
-      //   gridHorizontal.push(<div key={indexHorizontal}>{indexHorizontal}h</div>)
-      //   indexHorizontal++
-      // }
     }
   }, [])
 
 
 
   return (
-    <div ref={grid} className='grid__container'
-      style={{
-        // display: 'grid',
-        // gridTemplateColumns: 'repeat(5, 1fr)',
-      }}
-    >
-      {/* <div className='grid'></div> */}
-      {/* <div>{gridVertical}</div> */}
-      {/* <div>{gridHorizontal}</div> */}
+    <div ref={grid} className='grid__container'>
       <div
         style={{
           display: 'grid',
-          gridTemplateColumns: `repeat(${tileRows}, 1fr)`,
+          // gridTemplateColumns: `repeat(5, 1fr)`,
+          gridTemplateColumns: `repeat(${tileColumns}, 1fr)`,
         }}
       >{tileSets}</div>
     </div>
