1ae38ca15ac6fb783bd06f76ba67b3b0c360df76
wishba <wisnubayu34@gmail.com>
2023-02-25 10:30:15 +0700

    use function for coordinate

--- src/containers/Land.jsx
+++ src/containers/Land.jsx
@@ -3,22 +3,20 @@ import './Land.css'
 import c1_1 from '../assets/Grass hill tiles v.2.png'
 
 function Land() {
+  function landCoordinate([x, y]) {
+    const coordinate = {
+      top: `calc(17px * ${x} * 5)`,
+      left: `calc(17px * ${y} * 5)`,
+    }
+    return coordinate
+  }
+
   return (
     <div className='land'>
-      <div className='land__tile--container'
-        style={{
-          top: 'calc(17px * 1 * 5)',
-          left: 'calc(17px * 1 * 5)',
-        }}
-      >
+      <div className='land__tile--container' style={landCoordinate([1, 1])}>
         <img className='land__tile' src={c1_1} alt="tile" />
       </div>
-      <div className='land__tile--container'
-        style={{
-          top: 'calc(17px * 1 * 5)',
-          left: 'calc(17px * 2 * 5)',
-        }}
-      >
+      <div className='land__tile--container' style={landCoordinate([1, 2])}>
         <img className='land__tile' src={c1_1} alt="tile"
           style={{
             transform: 'translateX(calc(-1 * var(--map-tile-width)))',
