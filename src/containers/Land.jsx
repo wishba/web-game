38e64178238c74b744b7dc7155a88a2d5c17ddc4
wishba <wisnubayu34@gmail.com>
2023-02-25 10:48:18 +0700

    tileCoordinate function

--- src/containers/Land.jsx
+++ src/containers/Land.jsx
@@ -5,8 +5,15 @@ import c1_1 from '../assets/Grass hill tiles v.2.png'
 function Land() {
   function landCoordinate([x, y]) {
     const coordinate = {
-      top: `calc(17px * ${x} * 5)`,
-      left: `calc(17px * ${y} * 5)`,
+      left: `calc(17px * ${x} * 5)`,
+      top: `calc(17px * ${y} * 5)`,
+    }
+    return coordinate
+  }
+
+  function tileCoordinate([x, y]) {
+    const coordinate = {
+      transform: `translate(calc(${x} * var(--map-tile-width)), calc(${y} * var(--map-tile-width)))`,
     }
     return coordinate
   }
@@ -14,18 +21,15 @@ function Land() {
   return (
     <div className='land'>
       <div className='land__tile--container' style={landCoordinate([1, 1])}>
-        <img className='land__tile' src={c1_1} alt="tile"
-          style={{
-            transform: 'translate(0, 0)',
-          }}
-        />
+        <img className='land__tile' src={c1_1} alt="tile" style={tileCoordinate([0, 0])} />
       </div>
-      <div className='land__tile--container' style={landCoordinate([1, 2])}>
-        <img className='land__tile' src={c1_1} alt="tile"
-          style={{
-            transform: 'translate(calc(-1 * var(--map-tile-width)), 0)',
-          }}
-        />
+
+      <div className='land__tile--container' style={landCoordinate([2, 1])}>
+        <img className='land__tile' src={c1_1} alt="tile" style={tileCoordinate([-1, 0])} />
+      </div>
+
+      <div className='land__tile--container' style={landCoordinate([3, 1])}>
+        <img className='land__tile' src={c1_1} alt="tile" style={tileCoordinate([-2, 0])} />
       </div>
     </div>
   )
