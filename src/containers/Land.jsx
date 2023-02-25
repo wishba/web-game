5dd08cd21c59c8794210d9f781782cc0aeeb335b
wishba <wisnubayu34@gmail.com>
2023-02-25 09:49:49 +0700

    fix naming land class

--- src/containers/Land.jsx
+++ src/containers/Land.jsx
@@ -4,12 +4,12 @@ import c1_1 from '../assets/Grass hill tiles v.2.png'
 
 function Land() {
   return (
-    <div className='map'>
-      <div className='map__1-1'>
-        <img className='tile__1-1' src={c1_1} alt="tile" />
+    <div className='land'>
+      <div className='land__1-1--container'>
+        <img className='land__1-1' src={c1_1} alt="tile" />
       </div>
-      <div className='map__2-1'>
-        <img className='tile__2-1' src={c1_1} alt="tile" />
+      <div className='land__2-1--container'>
+        <img className='land__2-1' src={c1_1} alt="tile" />
       </div>
     </div>
   )
