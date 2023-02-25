3651dbba9900e6fe8f58696f7031df77c697d481
wishba <wisnubayu34@gmail.com>
2023-02-25 09:54:41 +0700

    add more variable

--- src/containers/Land.jsx
+++ src/containers/Land.jsx
@@ -5,10 +5,10 @@ import c1_1 from '../assets/Grass hill tiles v.2.png'
 function Land() {
   return (
     <div className='land'>
-      <div className='land__1-1--container'>
+      <div className='land__tile land__1-1--container'>
         <img className='land__1-1' src={c1_1} alt="tile" />
       </div>
-      <div className='land__2-1--container'>
+      <div className='land__tile land__2-1--container'>
         <img className='land__2-1' src={c1_1} alt="tile" />
       </div>
     </div>
