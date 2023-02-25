ce77f02f0e9461b3c20384322192191afeba4d3c
wishba <wisnubayu34@gmail.com>
2023-02-25 09:24:27 +0700

    tile 2-1

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,12 +5,11 @@ import c1_1 from '../assets/Grass hill tiles v.2.png'
 function Map() {
   return (
     <div className='map'>
-      <div className='map__tile'>
-        <img className='tile__1-1' src={c1_1} alt="tile"
-          style={{
-            // position: 'absolute'
-          }}
-        />
+      <div className='map__1-1'>
+        <img className='tile__1-1' src={c1_1} alt="tile" />
+      </div>
+      <div className='map__2-1'>
+        <img className='tile__2-1' src={c1_1} alt="tile" />
       </div>
     </div>
   )
