1692f6d664315c93d668707d9596debf2cc80631
wishba <wisnubayu34@gmail.com>
2023-02-26 09:11:50 +0700

    delete comment

--- src/containers/Land.jsx
+++ src/containers/Land.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import './Land.css'
-import c1_1 from '../assets/Grass hill tiles v.2.png'
+import tilesImage from '../assets/Grass hill tiles v.2.png'
 
 function Land() {
   function landCoordinate([x, y]) {
@@ -19,39 +19,28 @@ function Land() {
   }
 
   const landData = [[1, 1], [2, 1], [3, 1]]
-  // function name([x, y]) {
-  //   console.log([x, y]);
-  // }
   function landTile() {
     for (let index = 0; index < landData.length; index++) {
-      // for (let index1 = 0; index1 < landData[index].length; index1++) {
-      //   console.log(landData[index][index1]);
-      // }
-      // console.log(landData[index][0]);
-      // console.log(landData[index]);
-      console.log(`x:${landData[index][0]} and y:${landData[index][1]}`);
-      // name([1, 1])
+      console.log(`${index}) x:${landData[index][0]} and y:${landData[index][1]}`);
     }
   }
+
   useEffect(() => {
     landTile()
-    // console.log(landData[0][0]);
   }, [])
-
-
 
   return (
     <div className='land'>
       <div className='land__tile--container' style={landCoordinate([1, 1])}>
-        <img className='land__tile' src={c1_1} alt="tile" style={tileCoordinate([0, 0])} />
+        <img className='land__tile' src={tilesImage} alt="tile" style={tileCoordinate([0, 0])} />
       </div>
 
       <div className='land__tile--container' style={landCoordinate([2, 1])}>
-        <img className='land__tile' src={c1_1} alt="tile" style={tileCoordinate([-1, 0])} />
+        <img className='land__tile' src={tilesImage} alt="tile" style={tileCoordinate([-1, 0])} />
       </div>
 
       <div className='land__tile--container' style={landCoordinate([3, 1])}>
-        <img className='land__tile' src={c1_1} alt="tile" style={tileCoordinate([-2, 0])} />
+        <img className='land__tile' src={tilesImage} alt="tile" style={tileCoordinate([-2, 0])} />
       </div>
     </div>
   )
