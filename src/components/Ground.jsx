326b283400c5b50e34c0b2c9479ecd25ed5c2560
wishba <wisnubayu34@gmail.com>
2023-02-27 19:27:15 +0700

    render using loop

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -3,60 +3,66 @@ import './Ground.css'
 import groundAsset from '../assets/Grass tiles v.2.png'
 
 function ground() {
-  const [groundTile, setGroundTile] = useState()
-
   const groundArray = [
     [1, 1], [1, 2], [1, 3],
-    [2, 1], [2, 2], [2, 3],
-    [3, 1], [3, 2], [3, 3],
   ]
 
+  const [groundTile, setGroundTile] = useState()
+
   useEffect(() => {
-    const groundTileTotal = 3
     const groundTileArray = []
 
-    for (let index = 0; index < groundTileTotal; index++) {
+    for (let index = 0; index < groundArray.length; index++) {
+      const element = groundArray[index];
+      console.log(`${element[0]}:${element[1]}`);
+
       groundTileArray.push(
-        <div className='ground' key={`ground${index}`}>
+        <div key={index} className='ground'
+          style={{
+            top: `calc(85px * ${element[0]})`,
+            left: `calc(85px * ${element[1]})`,
+          }}
+        >
           <img className='ground__asset' src={groundAsset} alt="ground" />
         </div>
       )
     }
+
     setGroundTile(groundTileArray)
   }, [])
 
 
   return (
-    <>
-      {/* {groundTile} */}
+    <div>
+      {groundTile}
 
-      <div className='ground'
+      {/* <div className='ground'
         style={{
           top: `calc(85px * ${groundArray[0][0]})`,
           left: `calc(85px * ${groundArray[0][0]})`,
         }}
       >
         <img className='ground__asset' src={groundAsset} alt="ground" />
-      </div>
+      </div> */}
 
-      <div className='ground'
+      {/* <div className='ground'
         style={{
           top: `calc(85px * ${groundArray[1][0]})`,
           left: `calc(85px * ${groundArray[1][1]})`,
         }}
       >
         <img className='ground__asset' src={groundAsset} alt="ground" />
-      </div>
+      </div> */}
 
-      <div className='ground'
+      {/* <div className='ground'
         style={{
           top: `calc(85px * ${groundArray[2][0]})`,
           left: `calc(85px * ${groundArray[2][1]})`,
         }}
       >
         <img className='ground__asset' src={groundAsset} alt="ground" />
-      </div>
-    </>
+      </div> */}
+    </div>
   )
 }
 
