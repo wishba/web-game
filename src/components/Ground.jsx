0cb3ae0bb1067ebaea3af773998a7cbcf353292b
wishba <wisnubayu34@gmail.com>
2023-02-27 12:31:45 +0700

    ground position in coordinate

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,18 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Ground.css'
 import groundAsset from '../assets/Grass tiles v.2.png'
 
 function ground() {
+  const [groundTile, setGroundTile] = useState()
+
+  useEffect(() => {
+    const groundTileTotal = 3
+    const groundTileArray = []
+
+    for (let index = 0; index < groundTileTotal; index++) {
+      groundTileArray.push(
+        <div className='ground' key={`ground${index}`}>
+          <img className='ground__asset' src={groundAsset} alt="ground" />
+        </div>
+      )
+    }
+    setGroundTile(groundTileArray)
+  }, [])
+
+
   return (
-    <div className='ground__container'>
-      <div className='ground'>
+    <>
+      {/* {groundTile} */}
+
+      <div className='ground'
+        style={{
+          top: 'calc(85px * 1)',
+          left: 'calc(85px * 1)',
+        }}
+      >
         <img className='ground__asset' src={groundAsset} alt="ground" />
       </div>
 
-      <div className='ground'>
+      <div className='ground'
+        style={{
+          top: 'calc(85px * 1)',
+          left: 'calc(85px * 2)',
+        }}
+      >
         <img className='ground__asset' src={groundAsset} alt="ground" />
       </div>
-    </div>
+
+      <div className='ground'
+        style={{
+          top: 'calc(85px * 1)',
+          left: 'calc(85px * 3)',
+        }}
+      >
+        <img className='ground__asset' src={groundAsset} alt="ground" />
+      </div>
+    </>
   )
 }
 
