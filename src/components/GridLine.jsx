676f6dc8ca5de22186c15b9eb6ff35966b2d35ca
wishba <wisnubayu34@gmail.com>
2023-02-27 11:23:18 +0700

    grid tiles

--- src/components/GridLine.jsx
+++ src/components/GridLine.jsx
@@ -1,18 +1,39 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './GridLine.css'
 import Grid from './Grid'
 
 function GridLine() {
-  const gridLineRef = useRef(null)
+  const gridLineRef = useRef()
+  const [tiles, setTiles] = useState()
+  const [gridColumn, setGridColumn] = useState()
 
   useEffect(() => {
-    console.log(Math.floor(gridLineRef.current.offsetWidth / 16 / 5));
-    console.log(Math.floor(gridLineRef.current.offsetHeight / 16 / 5));
+    const tileHorizontal = Math.floor(gridLineRef.current.offsetWidth / 16 / 5)
+    const tileVertical = Math.floor(gridLineRef.current.offsetHeight / 16 / 5)
+
+    setGridColumn(tileHorizontal)
+
+    const tileArray = []
+    for (let indexY = 0; indexY < tileVertical; indexY++) {
+      for (let indexX = 0; indexX < tileHorizontal; indexX++) {
+        tileArray.push(<Grid key={`${indexY}/${indexX}`} coordinate={`${indexY}/${indexX}`} />)
+      }
+    }
+    setTiles(tileArray)
   }, [])
 
   return (
-    <div ref={gridLineRef} className='gridLine'>
-      <Grid coordinate="tes" />
+    <div ref={gridLineRef}>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${gridColumn}, auto)`,
+          width: 'fit-content',
+          height: '100vh',
+        }}
+      >
+        {tiles}
+      </div>
     </div>
   )
 }
