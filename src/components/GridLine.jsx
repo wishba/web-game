5c53ec2e20b09591b0035e502222e77acac73e9d
wishba <wisnubayu34@gmail.com>
2023-02-27 10:39:05 +0700

    log screen width

--- src/components/GridLine.jsx
+++ src/components/GridLine.jsx
@@ -1,12 +1,18 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './GridLine.css'
 import Grid from './Grid'
 
 function GridLine() {
   const gridLineRef = useRef(null)
+  useEffect(() => {
+    console.log(Math.floor(gridLineRef.current.offsetWidth / 16));
+    console.log(Math.floor(gridLineRef.current.offsetHeight / 16));
+  }, [])
 
   return (
-    <div ref={gridLineRef} className='gridLine'>
+    <div ref={gridLineRef} className='gridLine'
+      style={{ border: '1px solid red', height: '100vh' }}
+    >
       <Grid coordinate="tes" />
     </div>
   )
