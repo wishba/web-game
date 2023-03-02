a06974e7bfaffb13061b5c514b51ca22181d6da0
wishba <wisnubayu34@gmail.com>
2023-03-02 23:37:47 +0700

    del com

--- src/components/GroundBridge.jsx
+++ src/components/GroundBridge.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import './GroundBridge.css'
-import bridgeAsset from '../assets/Wood Bridge.png'
-import { placeTile } from '../utils/placeTile'
 import Tile from './Tile'
 
 function GroundBridge() {
@@ -13,14 +11,8 @@ function GroundBridge() {
   ]
 
   const [bridgeTile, setBridgeTile] = useState()
-  const [test, setTest] = useState()
 
   useEffect(() => {
-    const asset = <img src={bridgeAsset} alt="bridge asset" />
-    // const asset = 'tes'
-    // console.log(bridgeArray.length);
-    setTest(placeTile(bridgeArray, asset))
-
     const groundTileArray = []
 
     for (let index = 0; index < bridgeArray.length; index++) {
@@ -31,18 +23,6 @@ function GroundBridge() {
           <Tile tes1={index} tes2={element} tes3={tileArray} />
         </div>
       )
-      // groundTileArray.push(
-      //   <div key={index} className='groundBridge'
-      //     style={{
-      //       top: `calc(85px * ${element[0]})`,
-      //       left: `calc(85px * ${element[1]})`,
-      //     }}
-      //   >
-      //     <img className='groundBridge__asset' src={bridgeAsset} alt="ground"
-      //       style={{ transform: `translate(calc(-16px * 5 * ${tileArray[index][0]}), calc(-16px * 5 * ${tileArray[index][1]}))` }}
-      //     />
-      //   </div>
-      // )
     }
 
     setBridgeTile(groundTileArray)
@@ -51,7 +31,6 @@ function GroundBridge() {
   return (
     <div>
       {bridgeTile}
-      {/* <p>{test}</p> */}
     </div>
   )
 }
