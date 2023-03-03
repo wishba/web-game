9fc24ccfdc07f734658424de06cd572b914bc341
wishba <wisnubayu34@gmail.com>
2023-03-03 09:30:38 +0700

    OneIsland

--- src/components/object/Object.jsx
+++ src/components/object/Object.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Bridge from './Bridge'
+import OneIsland from './OneIsland'
 import TwoIsland from './TwoIsland'
 
 function Object() {
   return (
     <div>
+      <OneIsland />
       <Bridge />
       <TwoIsland />
     </div>
---
+++ src/components/object/OneIsland.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ObjectTile from './ObjectTile'
+import grassAsset from '../../assets/Grass tiles v.2.png'
+
+function OneIsland() {
+  return (
+    <>
+      <ObjectTile asset={grassAsset} assetWidth={176} />
+    </>
+  )
+}
+
+export default OneIsland
