3ea6a290b57bdc0d0cf1781616d3a3023e5d8762
wishba <wisnubayu34@gmail.com>
2023-03-03 07:30:10 +0700

    rename

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,48 +1,48 @@
 import React, { useEffect, useState } from 'react'
 import './Ground.css'
-import groundAsset from '../assets/Grass tiles v.2.png'
+import grassAsset from '../assets/Grass tiles v.2.png'
 
 function Ground() {
-  const groundArray = [
+  const placementArray = [
     [1, 1], [1, 2], [1, 3],
     [2, 1], [2, 2], [2, 3],
     [3, 1], [3, 2], [3, 3],
   ]
-  const tileArray = [
+  const assetArray = [
     [0, 0], [1, 0], [2, 0],
     [0, 1], [1, 1], [2, 1],
     [0, 2], [1, 2], [2, 2],
   ]
 
-  const [groundTile, setGroundTile] = useState()
+  const [tiles, setTiles] = useState()
 
   useEffect(() => {
-    const groundTileArray = []
+    const tileArray = []
 
-    for (let index = 0; index < groundArray.length; index++) {
-      const element = groundArray[index];
+    for (let index = 0; index < placementArray.length; index++) {
+      const element = placementArray[index];
 
-      groundTileArray.push(
+      tileArray.push(
         <div key={index} className='ground'
           style={{
             top: `calc(85px * ${element[0]})`,
             left: `calc(85px * ${element[1]})`,
           }}
         >
-          <img className='ground__asset' src={groundAsset} alt="ground"
-            style={{ transform: `translate(calc(-16px * 5 * ${tileArray[index][0]}), calc(-16px * 5 * ${tileArray[index][1]}))` }}
+          <img className='ground__asset' src={grassAsset} alt="ground"
+            style={{ transform: `translate(calc(-16px * 5 * ${assetArray[index][0]}), calc(-16px * 5 * ${assetArray[index][1]}))` }}
           />
         </div>
       )
     }
 
-    setGroundTile(groundTileArray)
+    setTiles(tileArray)
   }, [])
 
 
   return (
     <div>
-      {groundTile}
+      {tiles}
     </div>
   )
 }
