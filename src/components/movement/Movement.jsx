76dd30090348a8ec2d58e4b3839c1efb482e6130
wishba <wisnubayu34@gmail.com>
2023-03-04 21:06:47 +0700

    handleMove

--- src/components/movement/Movement.jsx
+++ src/components/movement/Movement.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Movement.css'
 import Camera from '../camera/Camera'
 
 function Movement() {
+  const [positionX, setPositionX] = useState(0)
+  const [positionY, setPositionY] = useState(0)
+  const intervalId = useRef()
+
+  function handleMoveStart(direction) {
+    intervalId.current = setInterval(() => {
+      switch (direction) {
+        case 'up':
+          return setPositionY(positionY => positionY + 1)
+        case 'left':
+          return setPositionX(positionX => positionX + 1)
+        case 'right':
+          return setPositionX(positionX => positionX - 1)
+        case 'down':
+          return setPositionY(positionY => positionY - 1)
+      }
+    }, 25)
+  }
+  function handleMoveStop() {
+    clearInterval(intervalId.current)
+    console.log(`${positionX}/${positionY}`);
+  }
+
   return (
     <>
-      <Camera cameraX={0} cameraY={0} />
+      <Camera cameraX={positionX} cameraY={positionY} />
       <div className='movement'>
-        <button className='movement__button movement__button--up'>
+        <button className='movement__button movement__button--up'
+          onMouseDown={() => handleMoveStart('up')}
+          onMouseUp={handleMoveStop}
+        >
           <p className='movement__arrow'>&#8593;</p>
         </button>
-        <button className='movement__button movement__button--left'>
+        <button className='movement__button movement__button--left'
+          onMouseDown={() => handleMoveStart('left')}
+          onMouseUp={handleMoveStop}
+        >
           <p className='movement__arrow'>&#8593;</p>
         </button>
-        <button className='movement__button movement__button--right'>
+        <button className='movement__button movement__button--right'
+          onMouseDown={() => handleMoveStart('right')}
+          onMouseUp={handleMoveStop}>
           <p className='movement__arrow'>&#8593;</p>
         </button>
-        <button className='movement__button movement__button--down'>
+        <button className='movement__button movement__button--down'
+          onMouseDown={() => handleMoveStart('down')}
+          onMouseUp={handleMoveStop}>
           <p className='movement__arrow'>&#8593;</p>
         </button>
       </div>
