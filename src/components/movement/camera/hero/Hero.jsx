ad6a6a983fd35df57b48972805436977179fcb3d
wishba <wisnubayu34@gmail.com>
2023-03-06 10:11:13 +0700

    facing class

--- src/components/movement/camera/hero/Hero.jsx
+++ src/components/movement/camera/hero/Hero.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './Hero.css'
 import heroAsset from '../../../../assets/Basic Charakter Spritesheet.png'
 
-function Hero() {
+function Hero({ face }) {
   return (
     <div className='hero__container'>
-      <img className='hero' src={heroAsset} alt="hero" />
+      <img className={`hero ${face}`} src={heroAsset} alt="hero" />
+      {/* <img className='hero up-1' src={heroAsset} alt="hero" /> */}
     </div>
   )
 }
