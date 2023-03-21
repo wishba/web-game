612b80703fb523cf9df3f13cc7b9dfab1201a699
wishba <wisnubayu34@gmail.com>
2023-03-21 19:32:13 +0700

    letter button

--- src/App.jsx
+++ src/App.jsx
@@ -267,7 +267,29 @@ function App() {
         /> */}
 
         {/* <div> */}
-        <Letter display={dialogueLetter} />
+        {/* <Letter display={dialogueLetter} /> */}
+        <div className='app__letter' style={{ display: dialogueLetter }} >
+          <br />
+          <p>Dear Adventurer,</p>
+          <br />
+          <p>
+            To claim your treasure, you must cross the narrow and dangerous bridge that
+            connects our island to the mainland. The sea below is infested with dangerous
+            creatures, and we urge you to stay away from the edges. Though the journey is
+            perilous, the treasure is worth the risk. May fortune favor the brave.
+          </p>
+          <br />
+          <p>Sincerely,</p>
+          <p>The Guardians of the Treasure Island</p>
+          <br />
+          <button
+            onClick={() => {
+              setDialogueLetter('none')
+              // setDialogue('none')
+              handleButtonX()
+            }}
+          >close(x)</button>
+        </div>
         {/* </div> */}
         {/* <Letter display={'none'} /> */}
 
