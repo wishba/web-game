efd23c8488f4d58761017f760ff0143256906973
wishba <wisnubayu34@gmail.com>
2023-09-05 14:27:21 +0700

    make letter into its own component

---
+++ src/components/Letter.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Letter() {
+  return (
+    <>
+      <br />
+      <p>Dear Adventurer,</p>
+      <br />
+      <p>
+        To claim your treasure, you must cross the narrow and dangerous bridge that
+        connects our island to the mainland. The sea below is infested with dangerous
+        creatures, and we urge you to stay away from the edges. Though the journey is
+        perilous, the treasure is worth the risk. May fortune favor the brave.
+      </p>
+      <br />
+      <p>Sincerely,</p>
+      <p>The Guardians of the Treasure Island</p>
+      <br />
+    </>
+  )
+}
+
+export default Letter
